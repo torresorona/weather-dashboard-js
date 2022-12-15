794a435f7277c37d9d799af0efc4d4987b769609
Carlos Torres Orona <ce.torresorona@gmail.com>
2022-12-15 11:32:35 -0500

    Completed getCityCoordinates and getWeatherResponse functions to receive expected data

--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,20 @@ $(function () {
     var formEl = $("#city-form");
 
     
-    function getWeatherResponse() {
-        weatherAPIURL = "api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
+    function getWeatherResponse(geoData) {
+        weatherAPIURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
+        console.log(weatherAPIURL);
+        fetch(weatherAPIURL)
+            .then(function (response) {
+                if (response.status !== 200) {
+                    console.log("No good")
+                }
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                
+            })
     }
 
     function getCityCoordinates(e) {
@@ -19,6 +31,20 @@ $(function () {
         // Query string to find 
         var geocodingURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityRequested + "&limit=1&appid=" + APIkey;
 
+        fetch(geocodingURL)
+            .then(function (response) {
+                if (response.status !== 200) {
+                    console.log("No good")
+                }
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data[0].lat);
+                lat = data[0].lat;
+                console.log(data[0].lon);
+                lon = data[0].lon;
+                getWeatherResponse(data);
+            })
 
     }
 
